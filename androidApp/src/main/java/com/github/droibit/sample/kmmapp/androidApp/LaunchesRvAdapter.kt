05f27ead054ee92ae9f6dd207fa144b1ad6dae45
Shinya Kumagai <roomful.rooms@gmail.com>
2020-09-16 19:01:46 +0900

    Update layouts of android app

---
+++ androidApp/src/main/java/com/github/droibit/sample/kmmapp/androidApp/LaunchesRvAdapter.kt
@@ -0,0 +1,66 @@
+package com.github.droibit.sample.kmmapp.androidApp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.github.droibit.sample.kmmapp.shared.entity.RocketLaunch
+
+class LaunchesRvAdapter(private val launched: List<RocketLaunch>) :
+    ListAdapter<RocketLaunch, LaunchViewHolder>(
+        ITEM_CALLBACK
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaunchViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return inflater.inflate(R.layout.item_launch, parent, false)
+            .run(::LaunchViewHolder)
+    }
+
+    override fun onBindViewHolder(holder: LaunchViewHolder, position: Int) {
+        holder.bindData(launched[position])
+    }
+
+    companion object {
+        private val ITEM_CALLBACK = object : DiffUtil.ItemCallback<RocketLaunch>() {
+            override fun areItemsTheSame(oldItem: RocketLaunch, newItem: RocketLaunch): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: RocketLaunch, newItem: RocketLaunch): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class LaunchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val missionNameTextView = itemView.findViewById<TextView>(R.id.missionName)
+    private val launchYearTextView = itemView.findViewById<TextView>(R.id.launchYear)
+    private val launchSuccessTextView = itemView.findViewById<TextView>(R.id.launchSuccess)
+    private val missionDetailsTextView = itemView.findViewById<TextView>(R.id.details)
+
+    fun bindData(launch: RocketLaunch) {
+        val ctx = itemView.context
+        missionNameTextView.text = ctx.getString(R.string.mission_name_field, launch.missionName)
+        launchYearTextView.text = ctx.getString(R.string.launch_year_field, launch.launchYear.toString())
+        missionDetailsTextView.text = ctx.getString(R.string.details_field, launch.details ?: "")
+        val launchSuccess = launch.launchSuccess
+        if (launchSuccess != null ) {
+            if (launchSuccess) {
+                launchSuccessTextView.text = ctx.getString(R.string.successful)
+                launchSuccessTextView.setTextColor((ContextCompat.getColor(itemView.context, R.color.colorSuccessful)))
+            } else {
+                launchSuccessTextView.text = ctx.getString(R.string.unsuccessful)
+                launchSuccessTextView.setTextColor((ContextCompat.getColor(itemView.context, R.color.colorUnsuccessful)))
+            }
+        } else {
+            launchSuccessTextView.text = ctx.getString(R.string.no_data)
+            launchSuccessTextView.setTextColor((ContextCompat.getColor(itemView.context, R.color.colorNoData)))
+        }
+    }
+}
--- androidApp/src/main/java/com/github/droibit/sample/kmmapp/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/github/droibit/sample/kmmapp/androidApp/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.github.droibit.sample.kmmapp.androidApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.droibit.sample.kmmapp.shared.Greeting
-import android.widget.TextView
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.coroutines.MainScope
 
-fun greet(): String {
-    return Greeting().greeting()
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-class MainActivity : AppCompatActivity() {
+    private val mainScope = MainScope()
+
+    private lateinit var launchesRecyclerView: RecyclerView
+    private lateinit var progressBarView: FrameLayout
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
     }
 }
